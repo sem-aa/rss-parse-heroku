f035a9cf422716d7504ae2f03da934e9d85311c1
sem-aa <66129330+sem-aa@users.noreply.github.com>
2023-05-06 19:20:11 +0300

    fix login

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -5,7 +5,7 @@ const config = { withCredentials: true };
 
 export const login = async (credentials) => {
   try {
-    const response = await axios.post("api/login", credentials, config);
+    const response = await axios.post("api/auth/login", credentials, config);
     return response.data;
   } catch (err) {
     throw new Error(err.response.data.message);
