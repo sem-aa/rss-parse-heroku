b4cdb66d0ff259d9351170737b4bc3f560bf5376
sem-aa <66129330+sem-aa@users.noreply.github.com>
2023-05-06 19:40:58 +0300

    try find prombel

--- client/src/api/posts.js
+++ client/src/api/posts.js
@@ -27,6 +27,7 @@ export const fetchPosts = async (params) => {
 export const fetchPostById = async (id) => {
   try {
     const response = await axios.get(`api/posts/${id}`);
+    console.log("by id", response);
     return response.data;
   } catch (error) {
     console.error("Error fetching post by ID:", error);
